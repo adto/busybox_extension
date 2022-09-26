c2e05f9aa44b01804d7d3505705e8c1ef7497e80
Adam Toth <adam.toth@elektrobit.com>
2022-09-26 12:10:43 +0200

    realm client added

--- realm_client/realm_client.c
+++ realm_client/realm_client.c
@@ -26,12 +26,55 @@
 //usage:       "TBD"
 
 #include "libbb.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
-/* This is a NOFORK applet. Be very careful! */
+#define REALM_MAJOR 1
+#define REALM_MINOR 1
+#define WR_VALUE _IOW(REALM_MINOR,'a',int32_t*)
+#define RD_VALUE _IOR(REALM_MINOR,'b',int32_t*)
+#define ENTER_REALM _IO(REALM_MINOR,'c')
+#define EXIT_REALM _IO(REALM_MINOR,'d')
+
 
 int realm_client_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
 int realm_client_main(int argc UNUSED_PARAM, char **argv UNUSED_PARAM)
 {
-	printf("All hope abandon ye who enter the Realm here!\n");
+
+    int fd;
+    int32_t value, number=123;
+
+
+    printf("\nOpening realm driver\n");
+    fd = open("/dev/realm", O_RDWR);
+    if(fd < 0) {
+            printf("Cannot open realm device file...\n");
+            return 0;
+    }
+
+	printf("Enter the realm\n");
+	ioctl(fd, ENTER_REALM);
+
+    printf("Enter the Value to send\n");
+    //scanf("%d",&number);
+    printf("Writing Value to Driver: 123\n");
+    ioctl(fd, WR_VALUE, (int32_t*) &number);
+
+    printf("Reading Value from Driver\n");
+    ioctl(fd, RD_VALUE, (int32_t*) &value);
+    printf("Value is %d\n", value);
+
+	printf("Exit from realm\n");
+	ioctl(fd, EXIT_REALM);
+
+    printf("Closing Driver\n");
+    close(fd);
+
 	return EXIT_SUCCESS;
 }
